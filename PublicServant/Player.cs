2ba939ffcb8edcc0e2e71031b5a54275dfabcd82
Catherine Bradley <catherinetybradley@gmail.com>
2020-02-16 22:09:00 -0800

    add diagonal movement and WASD

--- PublicServant/Player.cs
+++ PublicServant/Player.cs
@@ -14,10 +14,12 @@ namespace PublicServant.Desktop
         public Texture2D servantTexture;
         public Vector2 servantPosition;
         public float servantSpeed;
+        private float diagonal;
 
         public Player()
         {
             servantSpeed = 300f;
+            diagonal = servantSpeed * (float)0.75;
         }
 
         public void PlayerPosition(GraphicsDeviceManager graphics) {
@@ -30,23 +32,55 @@ namespace PublicServant.Desktop
         )
         {
             var kstate = Keyboard.GetState();
+            var gameSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
             //servant movement and speed
-            if (kstate.IsKeyDown(Keys.Up))
-                servantPosition.Y -= servantSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Down))
-                servantPosition.Y += servantSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Left))
-                servantPosition.X -= servantSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            if (kstate.IsKeyDown(Keys.Right))
-                servantPosition.X += servantSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            //NorthEast Movement
+            if (kstate.IsKeyDown(Keys.W) && kstate.IsKeyDown(Keys.D))
+            {
+                servantPosition.Y -= diagonal * gameSeconds;
+                servantPosition.X += diagonal * gameSeconds;
+            }
+            //NorthWest Movement
+            else if (kstate.IsKeyDown(Keys.W) && kstate.IsKeyDown(Keys.A))
+            {
+                servantPosition.Y -= diagonal * gameSeconds;
+                servantPosition.X -= diagonal * gameSeconds;
+            }
+            //SouthEast Movement
+            else if (kstate.IsKeyDown(Keys.S) && kstate.IsKeyDown(Keys.D))
+            {
+                servantPosition.Y += diagonal * gameSeconds;
+                servantPosition.X += diagonal * gameSeconds;
+            }
+            //SouthWest Movement
+            else if (kstate.IsKeyDown(Keys.S) && kstate.IsKeyDown(Keys.A))
+            {
+                servantPosition.Y += diagonal * gameSeconds;
+                servantPosition.X -= diagonal * gameSeconds;
+            }
+            //North Movement
+            else if (kstate.IsKeyDown(Keys.W))
+            {
+                servantPosition.Y -= servantSpeed * gameSeconds;
+            }
+            //East Movement
+            else if (kstate.IsKeyDown(Keys.D)) 
+            {
+                servantPosition.X += servantSpeed * gameSeconds;
+            }
+            //South Movement
+            else if (kstate.IsKeyDown(Keys.S))
+            {
+                servantPosition.Y += servantSpeed * gameSeconds;
+            }
+            //West Movement
+            else if (kstate.IsKeyDown(Keys.A))
+            {
+                servantPosition.X -= servantSpeed * gameSeconds;
+            }
 
             //servant cannot leave window
-            if (kstate.IsKeyDown(Keys.Right))
-                servantPosition.X += servantSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
             if (servantPosition.X > graphics.PreferredBackBufferWidth - servantTexture.Width / 2)
                 servantPosition.X = graphics.PreferredBackBufferWidth - servantTexture.Width / 2;
             else if (servantPosition.X < servantTexture.Width / 2)
